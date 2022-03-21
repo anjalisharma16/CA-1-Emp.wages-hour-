6932fa9771010e4145e65e9e6d4d7192ebfef277
anjalisharma16 <niyasharma642@gmail.com>
2022-03-21 20:23:34 +0530

    added PRSI and Net tax

--- ca.py
+++ ca.py
@@ -1,5 +1,5 @@
 #create employee class and attributes
-class Employee():
+class Employee:
 
     def __init__(self, staffId, Firstname, Lastname, Reghours, Hourlyrate, OTMultiple, Taxcredit, Standardband):
         self.staffId = int(staffId)
@@ -15,19 +15,21 @@ class Employee():
         salary = dict()
         salary['Name'] = self.Firstname + " "
         salary['Date'] = Date
-        salary['RHW'] = min(Hoursworked, self.Reghours)#Regular Hours Worked
-        salary['OHW'] = max(0, Hoursworked - self.Reghours)#OverTime Hours Worked
+        salary['RHW'] = min(Hoursworked, self.Reghours)#To calculate Regular Hours Worked
+        salary['OHW'] = max(0, Hoursworked - self.Reghours)#To calculate OverTime Hours Worked
         salary['Regular rate'] = self.Hourlyrate
-        salary['Overtime rate'] = self.Hourlyrate * self.OTMultiple
-        salary['Regular pay'] = self.Hourlyrate * salary['RHW']
-        salary['Overtime pay'] = max(salary['Overtime rate'] * salary['OHW'], 0)
-        salary['Gross pay'] = salary['Regular pay'] + salary['Overtime pay']
+        salary['Overtime rate'] = self.Hourlyrate * self.OTMultiple#To calculate Overtime rate
+        salary['Regular pay'] = self.Hourlyrate * salary['RHW']#To calculate Regular pay
+        salary['Overtime pay'] = max(salary['Overtime rate'] * salary['OHW'], 0)#To calculate Overtime pay
+        salary['Gross pay'] = salary['Regular pay'] + salary['Overtime pay']#To calculate Gross pay
         salary['Standard rate pay'] = self.Standardband
-        salary['Higher rate pay'] = max(salary['Gross pay'] - self.Standardband, 0)
-        salary['Standard tax'] = round(salary['Standard rate pay'] * 0.2, 2)
-        salary['Higher tax'] = round(salary['Higher rate pay'] * 0.4, 2)
-        salary['Total tax'] = salary['Standard tax'] + salary['Higher tax']
+        salary['Higher rate pay'] = max(salary['Gross pay'] - self.Standardband, 0)#To calculate Higher rate pay
+        salary['Standard tax'] = salary['Standard rate pay'] * 0.2#To calculate Standard tax
+        salary['Higher tax'] = round(salary['Higher rate pay'] * 0.4, 2)#To calculate Higher tax
+        salary['Total tax'] = round(salary['Standard tax'] + salary['Higher tax'], 2)#To calculate Total tax 
         salary['Tax credit'] = self.Taxcredit
-        salary['Net deductions'] = salary['Higher tax'] + salary['Tax credit']
-        salary['Net pay'] = max(0, salary['Gross pay'] - salary['Net deductions'])
+        salary['Net deductions'] = round(salary['Net tax'] + salary['PRSI'], 2)#To calculate Net deduction
+        salary['Net pay'] = max(0, salary['Gross pay'] - salary['Net deductions'])#To calculate Net pay
+        salary['Net tax'] =round(salary['Total tax'] - salary['Tax credit'], 2)#To calculate Net tax
+        salary['PRSI'] = salary['Gross pay'] * 0.4#To calculate PRSI
         return salary
